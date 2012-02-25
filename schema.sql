edd189115e70fae3a0bcec5d9cfac394e816e199
Tony Webster <tonywebster@gmail.com>
2012-02-25 20:00:46 +0000

    Updating schema

--- schema.sql
+++ schema.sql
@@ -1,165 +1,129 @@
--------------------------------------------------------------
--- MDB Tools - A library for reading MS Access database files
--- Copyright (C) 2000-2004 Brian Bruns
--- Files in libmdb are licensed under LGPL and the utilities under
--- the GPL, see COPYING.LIB and COPYING files respectively.
--- Check out http://mdbtools.sourceforge.net
--------------------------------------------------------------
-
-DROP TABLE Copy of tblViolationTypes;
-CREATE TABLE Copy of tblViolationTypes
+CREATE TABLE Copy_of_tblViolationTypes
  (
-	ViolationTypeID			Long Integer, 
-	ViolationType			Text (300), 
-	ViolationCategoryID			Long Integer, 
-	ViolationDeduction			Long Integer, 
-	RiskCategory			Text (100)
+	ViolationTypeID			int, 
+	ViolationType			varchar (300), 
+	ViolationCategoryID			int, 
+	ViolationDeduction			int, 
+	RiskCategory			varchar (100)
 );
--- CREATE ANY INDEXES ...
 
-DROP TABLE tble_violexplanation;
 CREATE TABLE tble_violexplanation
  (
-	ViolationTypeID			Long Integer, 
-	ViolationType			Text (300), 
-	RiskCategory			Text (100), 
-	ViolExplanation			Memo/Hyperlink (255)
+	ViolationTypeID			int, 
+	ViolationType			varchar (300), 
+	RiskCategory			varchar (100), 
+	ViolExplanation			text (255)
 );
--- CREATE ANY INDEXES ...
 
-DROP TABLE tblInspections;
 CREATE TABLE tblInspections
  (
-	InspectionID			Long Integer, 
-	DateOfInspection			DateTime (Short), 
-	UnitNo			Text (100), 
-	Total Time			Long Integer, 
-	InspectionTypeID			Long Integer, 
-	LocationIDInspected			Long Integer, 
-	InspectionNotes			Memo/Hyperlink (255), 
-	EmployeeIDofInspector			Long Integer, 
-	ComplaintIDforInspection			Long Integer, 
-	DataLastChangedByEmpID			Long Integer, 
-	PrimaryActionTaken			Text (100), 
-	SecondaryActionTaken			Text (100), 
-	OutcomeTaken			Text (100), 
-	Action1			Text (150), 
-	Action2			Text (150), 
-	Score			Long Integer, 
-	greensurveyid			Long Integer, 
-	HighRisk			Boolean, 
-	T_Stamp			Unknown 0x09, 
-	Account_ID			Long Integer
+	InspectionID			int, 
+	DateOfInspection			date, 
+	UnitNo			varchar (100), 
+	Total_Time			int, 
+	InspectionTypeID			int, 
+	LocationIDInspected			int, 
+	InspectionNotes			text (255), 
+	EmployeeIDofInspector			int, 
+	ComplaintIDforInspection			int, 
+	DataLastChangedByEmpID			int, 
+	PrimaryActionTaken			varchar (100), 
+	SecondaryActionTaken			varchar (100), 
+	OutcomeTaken			varchar (100), 
+	Action1			varchar (150), 
+	Action2			varchar (150), 
+	Score			int, 
+	greensurveyid			int, 
+	HighRisk			char, 
+	T_Stamp			varchar (8), 
+	Account_ID			int
 );
--- CREATE ANY INDEXES ...
 
-DROP TABLE tblInspectionTypes;
 CREATE TABLE tblInspectionTypes
  (
-	InspectionTypeID			Long Integer, 
-	InspectionType			Text (150)
+	InspectionTypeID			int, 
+	InspectionType			varchar (150)
 );
--- CREATE ANY INDEXES ...
 
-DROP TABLE tblInspectionTypes_old;
 CREATE TABLE tblInspectionTypes_old
  (
-	InspectionTypeID			Long Integer, 
-	InspectionType			Text (150)
+	InspectionTypeID			int, 
+	InspectionType			varchar (150)
 );
--- CREATE ANY INDEXES ...
 
-DROP TABLE TblInspName;
 CREATE TABLE TblInspName
  (
-	Fname			Text (100), 
-	Lname			Text (100), 
-	Phone			Text (100), 
-	District			Long Integer, 
-	SubDistrict			Long Integer
+	Fname			varchar (100), 
+	Lname			varchar (100), 
+	Phone			varchar (100), 
+	District			int, 
+	SubDistrict			int
 );
--- CREATE ANY INDEXES ...
 
-DROP TABLE tblLocations;
 CREATE TABLE tblLocations
  (
-	LocationID			Long Integer, 
-	DBA			Text (200), 
-	StreetAddress			Text (150), 
-	City			Text (100), 
-	Zip			Text (100), 
-	Type Description			Text (200), 
-	Census			Text (100), 
-	SubDistrict			Long Integer, 
-	Excellence			Text (510), 
-	District			Long Integer, 
-	Status			Text (140), 
-	LocationTypeID			Long Integer, 
-	symboltext			Text (200)
+	LocationID			int, 
+	DBA			varchar (200), 
+	StreetAddress			varchar (150), 
+	City			varchar (100), 
+	Zip			varchar (100), 
+	Type_Description			varchar (200), 
+	Census			varchar (100), 
+	SubDistrict			int, 
+	Excellence			varchar (510), 
+	District			int, 
+	Status			varchar (140), 
+	LocationTypeID			int, 
+	symboltext			varchar (200)
 );
--- CREATE ANY INDEXES ...
 
-DROP TABLE tblLocationtypes;
 CREATE TABLE tblLocationtypes
  (
-	LocationTypeID			Long Integer, 
-	Type Description			Text (200), 
-	ProgramID			Long Integer, 
-	TaxCode			Text (100), 
-	Minimum Yearly Routines			Long Integer, 
-	Inspection Difficulty Standard			Long Integer, 
-	RequireScore			Text (100), 
-	DescriptionSummary			Text (200)
+	LocationTypeID			int, 
+	Type_Description			varchar (200), 
+	ProgramID			int, 
+	TaxCode			varchar (100), 
+	Minimum_Yearly_Routines			int, 
+	Inspection_Difficulty_Standard			int, 
+	RequireScore			varchar (100), 
+	DescriptionSummary			varchar (200)
 );
--- CREATE ANY INDEXES ...
 
-DROP TABLE tblViolationCategories;
 CREATE TABLE tblViolationCategories
  (
-	ViolationCategoryID			Long Integer, 
-	Description			Text (100), 
-	T_Stamp			Unknown 0x09
+	ViolationCategoryID			int, 
+	Description			varchar (100), 
+	T_Stamp			varchar (8)
 );
--- CREATE ANY INDEXES ...
 
-DROP TABLE tblViolations;
 CREATE TABLE tblViolations
  (
-	ViolationID			Long Integer, 
-	InspectionID			Long Integer, 
-	ViolationCondition			Long Integer, 
-	DateDiscovered			DateTime (Short), 
-	LocationIDofViolation			Long Integer, 
-	ViolationSeverity			Text (100), 
-	ViolationTypeID			Long Integer, 
-	ViolationNotes			Memo/Hyperlink (255), 
-	DateAbated			DateTime (Short), 
-	InspectionIDofAbate			Long Integer, 
-	EmployeeID			Long Integer, 
-	DateCited			DateTime (Short), 
-	CheckToAbate			Boolean, 
-	abatecontrol			Boolean, 
-	T_Stamp			Unknown 0x09, 
-	SW			Text (10), 
-	Non_Closure			Boolean, 
-	Non_Closure_Date			DateTime (Short)
+	ViolationID			int, 
+	InspectionID			int, 
+	ViolationCondition			int, 
+	DateDiscovered			date, 
+	LocationIDofViolation			int, 
+	ViolationSeverity			varchar (100), 
+	ViolationTypeID			int, 
+	ViolationNotes			text (255), 
+	DateAbated			date, 
+	InspectionIDofAbate			int, 
+	EmployeeID			int, 
+	DateCited			date, 
+	CheckToAbate			char, 
+	abatecontrol			char, 
+	T_Stamp			varchar (8), 
+	SW			varchar (10), 
+	Non_Closure			char, 
+	Non_Closure_Date			date
 );
--- CREATE ANY INDEXES ...
 
-DROP TABLE tblViolationTypes;
 CREATE TABLE tblViolationTypes
  (
-	ViolationTypeID			Long Integer, 
-	ViolationType			Text (300), 
-	ViolationCategoryID			Long Integer, 
-	ViolationDeduction			Long Integer, 
-	RiskCategory			Text (100), 
-	T_Stamp			Unknown 0x09
+	ViolationTypeID			int, 
+	ViolationType			varchar (300), 
+	ViolationCategoryID			int, 
+	ViolationDeduction			int, 
+	RiskCategory			varchar (100), 
+	T_Stamp			varchar (8)
 );
--- CREATE ANY INDEXES ...
-
-
-
--- CREATE ANY Relationships ...
-
--- relationships are not supported for access
